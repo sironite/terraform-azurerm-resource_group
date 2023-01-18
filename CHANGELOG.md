0856c03a580f2baa83db79378fc8259ebd84e61b
WDamman <58424695+TheIronRock95@users.noreply.github.com>
2023-01-18 21:33:06 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# V1.0.0 - 2023-01-18
+
+feat: first release
+
+BREAKING CHANGE:
+- First Major Template release
---
+++ docs/header-doc.tf
@@ -0,0 +1,7 @@
+/**
+ *  # Module Name
+ * [![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](CHANGELOG.md) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](https://registry.terraform.io/)
+ *
+ * 
+ *
+ */
